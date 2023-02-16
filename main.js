d3a4264139c9750c25a3ea19dc3ac3e429df8abd
Monoster <123750573+W0cLxq@users.noreply.github.com>
2023-02-16 18:47:55 +0300

    Update main.js

--- main.js
+++ main.js
@@ -7,9 +7,11 @@ var exec_command = ""
 
 function create() {
     if (filetype.value == "small-file") {
+        exec_command = ""
         exec_command += "pkg --compress GZip --targets " + nodeversion.value + ostype.value + " " + GET_JS_PATH.value
         outputcode.value = exec_command
     } else if (filetype.value == "normal-file") {
+        exec_command = ""
         exec_command += "pkg --targets " + nodeversion.value + ostype.value + " " + GET_JS_PATH.value
         outputcode.value = exec_command
     }
