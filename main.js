32739c9b9bdd73a9fa5c350f3ea4fe954a7d5f30
Monoster <123750573+W0cLxq@users.noreply.github.com>
2023-02-18 11:51:31 +0300

    Update main.js

--- main.js
+++ main.js
@@ -7,24 +7,22 @@ var exec_command = ""
 
 function create() {
     if (filetype.value == "small-file") {
-        exec_command = ""
-        exec_command += "pkg --compress GZip --targets " + nodeversion.value + ostype.value + " " + GET_JS_PATH.value
+        exec_command = "pkg --compress GZip --targets " + nodeversion.value + ostype.value + " " + GET_JS_PATH.value
         outputcode.value = exec_command
     } else if (filetype.value == "normal-file") {
-        exec_command = ""
-        exec_command += "pkg --targets " + nodeversion.value + ostype.value + " " + GET_JS_PATH.value
+        exec_command = "pkg --targets " + nodeversion.value + ostype.value + " " + GET_JS_PATH.value
         outputcode.value = exec_command
     }
 }
 
 function copy_command() {
-    outputcode.select();
-    outputcode.setSelectionRange(0, 99999);
-    navigator.clipboard.writeText(outputcode.value);
+    outputcode.select()
+    outputcode.setSelectionRange(0, 99999)
+    navigator.clipboard.writeText(exec_command)
     outputcode.style.color = "White"
     outputcode.value = "You copied the code!"
     setTimeout(() => {
         outputcode.value = exec_command
         outputcode.style.color = "#624F82"
-    }, 1000);
+    }, 1000)
 }
